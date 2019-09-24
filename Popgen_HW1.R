5b67c23bf2016d88d806cdb9fb006c0b03f23baa
maccwinter <matthew.winter1@louisiana.edu>
2019-09-23 20:13:12 -0500

    calculated allele frequences of S,F, and I

--- Popgen_HW1.R
+++ Popgen_HW1.R
@@ -2,6 +2,27 @@
 
 
 #1B 
+#tot represents the total population number
+tot <- 141 + 111 + 15 + 28 + 32 + 5
+tot
+#The population total is 332 individuals 
+#allelefreq is a function to calculate the allele frequencies where x represents homozygotes where y and z represent the two different heterozygotes.
+allelefreq <- function(x,y,z) {(x + (y + z)/2)/tot}
+#fs represents the allele frequency of S
+fs <- allelefreq(141,111,15)
+fs
+#fs is 0.6144578
+#ff is the allele frequency for the F allele 
+ff <- allelefreq(28,32,111)
+ff
+#ff is 0.2996988
+#fi is the allele frequency of I
+fi <- allelefreq(5,32,15)
+fi
+#fi is 0.08584337
+#fs, fi, and ff need to add to 1
+fs + fi + ff
+#and they do :) 
 #ge is all the expected genotypes
 ge <- c(123.5,29.88,2.69,121.51,36.45,17.93)
 ge
